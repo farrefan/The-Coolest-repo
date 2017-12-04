6a7d3fce361b58cdbaededd8e650c5909bc32b7c
dorothywu <dw@D-10-157-220-232.dhcp4.washington.edu>
2017-12-04 12:23:34 -0800

    server.R

--- server.R
+++ server.R
@@ -1,7 +1,8 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
-
+library(plyr)
+library(ggplot2)
 
 data1 <- data.table::fread("data.cvs")
 server <- function(input, output) {
@@ -26,5 +27,17 @@ server <- function(input, output) {
     data2 <- dm %>% select(iyear,imonth,iday,attacktype1_txt,targtype1_txt,weaptype1_txt,city)
     data2
   },
-  options = list(lengthMenu = c(5, 10, 33), pageLength = 5))
+    options = list(lengthMenu = c(5, 10, 33), pageLength = 5))
+  
+  output$linechart <- renderPlot({
+    terrorism <- ddply(data1, .(iyear, country_txt), summarize, number=n())
+    specific_country <- dplyr::filter(terrorism, country_txt == input$country)
+    Number <- terrorism$number
+    Year <- unique(terro$iyear)
+    plot <- ggplot(data=specific_country) + 
+      geom_line(mapping=aes(x = iyear, y =number)) + 
+      geom_point(mapping=aes(x = iyear, y =number))+ 
+      labs(x = "Years", y = "Frequence")
+    plot
+  })
 }
--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ data1 <- data.table::fread("data.cvs")
 library(leaflet)
 library(shiny)
 
-
+country <- unique(data1$country_txt)
 ui <- fluidPage(
   titlePanel("Terrorism Map"),
   sidebarPanel(
@@ -10,14 +10,17 @@ ui <- fluidPage(
                 label = "Choose a country:",
                 choices = unique(data1$country_txt)),
     
-    selectInput(inputId = "yaer",
+    selectInput(inputId = "year",
                 label = "Choose a year:",
-                choices = unique(data1$iyear))),
-  
+                choices = unique(data1$iyear)),
+    radioButtons("country", label = "Country",
+               choices = country,
+               selected = 'United States')),
   mainPanel(
     textOutput("summary1"),
     leafletOutput(outputId = "distPlot1"), 
-    dataTableOutput(outputId = "distPlot2")
+    dataTableOutput(outputId = "distPlot2"),
+    plotOutput('linechart')
   )
   
 )  
