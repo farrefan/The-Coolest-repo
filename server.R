f7903b502b16ed7e0728ec69febbe332f79156a5
CHENGCHENG ZHU <568105514@qq.com>
2017-12-05 03:51:39 -0800

    new

--- server.R
+++ server.R
@@ -4,36 +4,43 @@ library(ggplot2)
 library(dplyr)
 
 data1 <- data.table::fread("data.cvs")
+names<-colnames(data1)
+names[2]<-"year"
+names[3]<-"month"
+names[4]<-"day"
+names[6]<-"country"
+names[11]<-"attack_type"
+names[12]<-"target_type"
+names[14]<-"weapon_type"
+colnames(data1)<-names
 server <- function(input, output) {
-  output$summary1 <- renderText({
-    print("This map reveals a geographic disstribution of terrorism activity.")
-  })
-  
+    
   output$distPlot1 <- renderLeaflet({
-    dm <- data1 %>% filter(iyear == input$yaer, country_txt == input$country)
+    dm <- data1 %>% filter(year == input$yaer, country == input$country)
     leaflet(data = dm) %>% 
       addTiles() %>% 
-      addMarkers(~longitude, ~latitude, popup = paste("Date:",dm$iyear,dm$imonth,dm$iday,"<br>",
-                                                      "Attact Type:",dm$attacktype1_txt,"<br>",
-                                                      "Target:", dm$targtype1_txt,"<br>",
+      addMarkers(~longitude, ~latitude, popup = paste("Date:",dm$year,dm$month,dm$day,"<br>",
+                                                      "Attact Type:",dm$attack_type,"<br>",
+                                                      "Target:", dm$target_type,"<br>",
                                                       "Motive:",dm$motive,"<br>",
-                                                      "Weapon Type:", dm$weaptype1_txt
-                                                      ))
+                                                      "Weapon Type:", dm$weapon_type
+      ))
   })
   
   output$distPlot2 <- renderDataTable({
-    dm <- data1 %>% filter(iyear == input$yaer, country_txt == input$country)
-    data2 <- dm %>% select(iyear, imonth, iday, attacktype1_txt, targtype1_txt, weaptype1_txt,city)
+    dm <- data1 %>% filter(year == input$yaer, country== input$country)
+    data2 <- dm %>% select(year, month, day, attack_type, target_type, weapon_type,city)
     data2
   },
-    options = list(lengthMenu = c(5, 10, 33), pageLength = 5))
+  options = list(lengthMenu = c(5, 10, 33), pageLength = 5))
   
   output$linechart <- renderPlot({
-    specific_country <- data1 %>% filter(country_txt == input$country) %>%
-      group_by(iyear) %>% dplyr::summarise(number = n())
+    specific_country <- data1 %>% filter(country == input$country) %>%
+      group_by(year) %>% dplyr::summarise(number = n())
     ggplot(data = specific_country) + 
-      geom_point(mapping = aes(x = iyear, y = number)) + 
-      geom_smooth(mapping = aes(x = iyear, y = number)) + 
-      labs(x = "Years", y = "Frequence")
+      geom_point(mapping = aes(x = year, y = number)) + 
+      geom_smooth(mapping = aes(x = year, y = number)) + 
+      labs(x = "Years", y = "Frequence")+
+      scale_x_continuous(limits=c(1970,2016),breaks=seq(1970,2016,2))
   })
 }
--- ui.R
+++ ui.R
@@ -1,19 +1,28 @@
 data1 <- data.table::fread("data.cvs")
 library(leaflet)
 library(shiny)
-
+names<-colnames(data1)
+names[2]<-"year"
+names[3]<-"month"
+names[4]<-"day"
+names[6]<-"country"
+names[11]<-"attack_type"
+names[12]<-"target_type"
+names[14]<-"weapon_type"
+colnames(data1)<-names
 
 ui <- fluidPage(
+  tags$h2("This map reveals a geographic disstribution of terrorism activity."),
   titlePanel("Terrorism Map"),
   sidebarPanel(
     selectInput(inputId = "country",
                 label = "Choose a country:",
-                choices = unique(data1$country_txt)),
+                choices = unique(data1$country)),
     
     selectInput(inputId = "yaer",
                 label = "Choose a year:",
-                choices = unique(data1$iyear))),
-
+                choices = unique(data1$year))),
+  
   mainPanel(
     textOutput("summary1"),
     leafletOutput(outputId = "distPlot1"), 
